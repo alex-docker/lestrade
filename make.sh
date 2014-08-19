fb891995e190e03853c543ff0c17b48ffe292f39
Brian Goff <cpuguy83@gmail.com>
2014-08-19 09:36:25 -0400

    Clean up static compile script

--- make.sh
+++ make.sh
@@ -1,16 +1,2 @@
-#!/bin/bash
-LDFLAGS_STATIC='-linkmode external'
-EXTLDFLAGS_STATIC='-static'
-BUILDFLAGS=( -a -tags "netgo static_build" )
-EXTLDFLAGS_STATIC="$EXTLDFLAGS_STATIC -lpthread -Wl,--unresolved-symbols=ignore-in-object-files"
-LDFLAGS_STATIC="
-  $LDFLAGS_STATIC
-  -extldflags \"$EXTLDFLAGS_STATIC\"
-"
 go get
-
-go build  "${BUILDFLAGS[@]}" \
-    -ldflags "
-        $LDFLAGS
-        $LDFLAGS_STATIC
-    "
+CGO_ENABLED=0 go build -a -ldflags -d
